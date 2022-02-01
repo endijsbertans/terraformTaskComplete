2b6521467dfe7fc74b52aded82a4bf667612a92b
Endijs Bertans <endijsbertans@gmail.com>
2022-02-01 16:41:46 +0200

    edited script file, removed echo

--- init-script.sh
+++ init-script.sh
@@ -2,7 +2,7 @@
 sudo chown -R ec2-user /var/log/
 cd /var/log/
 sudo dir > DataOfUploaded.txt
-echo 'konfigs hujova'
+
 aws configure set region eu-north-1
 aws s3 cp --recursive /var/log/. s3://endijsb
 aws configure list
