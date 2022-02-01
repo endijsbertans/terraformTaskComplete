a3472af2f0a9e9757c1080727f85c90365c6e3de
Endijs Bertans <endijsbertans@gmail.com>
2022-02-01 19:46:47 +0200

    updated iam script, easier to read

--- iam.tf
+++ iam.tf
@@ -1,29 +1,16 @@
 resource "aws_iam_role" "ec2_role" {
   name = "ec2_role"
 
-  assume_role_policy = "${file("ec2-assume.json")}"
+    assume_role_policy = "${file("ec2-assume.json")}"
     
-    inline_policy {
-        name = "ec2_policy"
-        policy = "${file("ex2-policy.json")}"
-    }
+      inline_policy {
+         name = "ec2_policy"
+         policy = "${file("ex2-policy.json")}"
+
+     }
+
   }
-  
   resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2_profile"
   role = "${aws_iam_role.ec2_role.name}"
-
 }
-# resource "aws_iam_policy" "ec2_policy" {
-#   name = "ec2_policy"
-# // role = "${aws_iam_role.ec2_role.id}"
-#   policy = "${file("ex2-policy.json")}"
-# }
-
-
-
-# resource "aws_iam_instance_profile" "ec2_profile" {
-#   name = "ec2_profile"
-#   role = "${aws_iam_role.ec2_role.name}"
-
-# }
